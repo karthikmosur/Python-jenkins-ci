b63d0cd6b0ef29d856f60741179196f7a0fc26c2
karthikmosur <42040681+karthikmosur@users.noreply.github.com>
2026-02-14 00:16:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 print("Hello from GitHub-triggered Jenkins build 🚀")
 print("New change detected by Jenkins 🎯")
 print("Hello from GitHub-triggered Jenkins build 22🚀")
+print("Hello from GitHub-triggered Jenkins build 24🚀")
 
